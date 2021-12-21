ab76a69dced21e8013f3a647ad6fedd2f367f7dd
cesarnr21 <cesarnr21@gmail.com>
2021-12-20 19:10:56 -0500

    initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # stm32-definitions
 Drivers and Headers files for STM32 Development
+
+**Inpiration**
+[Udemy: Embedded Systems BareMetal](https://www.udemy.com/course/embedded-systems-bare-metal-programming/)
---
+++ gpio.h
@@ -0,0 +1,23 @@
+/*
+ *
+ * GPIO Header File for the stm32f103c8t6 "Blue Pill" Microcontroller
+ *
+ * Author: Cesar Nunez Rodriguez
+ * 
+ * Developed on Summer 2021
+ * 
+ * Use this header file at your own risks
+ * I developed this header file for my own personal use
+ * and there is no warranty or guarantee that this file will work for your 
+ * application. When you use this code, you do so with full responsability
+ * 
+ */
+
+#define GPIO0
+#define GPIO1
+#define GPIO2
+#define GPIO3
+
+
+
+
